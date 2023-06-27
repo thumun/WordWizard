9b9fd724a2cbdfc4bbe9cfc56af345cef0dd25a9
nehathumu <nthumu@haverford.edu>
2023-06-27 16:51:32 -0400

    prototype mg3

---
+++ Assets/Scripts/MG3/MonsterBook.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBook : MonoBehaviour
+{
+
+    public string idiomKey; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        // lib book clicked 
+    }
+
+    void OnMouseOver()
+    {
+        // highlight ? 
+        
+    }
+
+}
