7b2136107a535866e472d4536bd8d0c2fdb97c13
Gavin-Sears <sgsears@haverford.edu>
2023-06-30 19:11:00 -0500

    Circle Menu Updates
    All menu parts have been made. Now they must be connected using the scripts.

---
+++ Assets/Scripts/MinGame2/QuestionInfo.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace MinigameTwo
+{
+    [Serializable]
+
+    public struct QuestionFormat
+    {
+        // Index of the first character of the word
+        // Does not count link tags
+        public int index;
+
+        // Index counting tags. May not be used
+        public int indexWithTags;
+
+        // Whether or not there is a mistake
+        public bool mistake;
+
+        // The word (space deliminated string) in question
+        public string word;
+
+        // Correct string that replaces original
+        public string correctAnswer;
+    }
+}
