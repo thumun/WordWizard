e0170695cd9733b94b7045a5be1640870f6cfa20
Gavin-Sears <sgsears@haverford.edu>
2023-07-20 14:24:02 -0500

    Building Zoom and Isometric movement
    Changed movement directions (for isometric) and added zoom and fade to black for building selection on map.

--- Assets/Scripts/NPCInteraction.cs
+++ Assets/Scripts/NPCInteraction.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NPCInteraction : MonoBehaviour
 {
+    public GameObject BlackScreen;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        BlackScreen = GameObject.Find("BlackScreen");
+        BlackScreen.SetActive(false);
     }
 
     // Update is called once per frame
@@ -28,7 +32,8 @@ public class NPCInteraction : MonoBehaviour
                 else if (hit.transform.gameObject.layer == 7)
                 {
                     Debug.Log("clicked on building");
-                    MiniGameStart(hit.transform.gameObject.name);
+                    StartCoroutine(BuildingZoom(hit.transform.gameObject, BlackScreen));
+                    //MiniGameStart(hit.transform.gameObject.name);
                 }
             }
         }
@@ -43,6 +48,52 @@ public class NPCInteraction : MonoBehaviour
     private void InitiateDialogue(string name)
     {
         // based on the name we get the dialogue info 
+    }
+
+    IEnumerator BuildingZoom(GameObject Building, GameObject blackScreen)
+    {
+        Vector3 CamPos = Camera.main.transform.position;
+        Vector3 BuildPos = Building.transform.position;
+        Vector3 correctBPos = new Vector3(BuildPos.x - 11.0f, BuildPos.y, BuildPos.z - 11.0f);
+        Vector3 newPos = new Vector3(0.0f, 0.0f, 0.0f);
+
+        blackScreen.SetActive(true);
+        Color c = blackScreen.GetComponent<Image>().color;
+        c.a = 0.0f;
+        blackScreen.GetComponent<Image>().color = c;
+
+
+        for (float f = 0.0f; f < 1.0; f += 0.01f)
+        {
+            newPos = correctBPos * f + CamPos * (1.0f - f);
+            Camera.main.transform.position = new Vector3(newPos.x, CamPos.y, newPos.z);
+
+            c.a = f;
+            blackScreen.GetComponent<Image>().color = c;
+
+            yield return new WaitForSeconds(0.01f);
+        }
+        MiniGameStart(Building.gameObject.name);
+
+    }
+
+    // https://www.youtube.com/watch?v=oNz4I0RfsEg 
+    IEnumerator FadeTransition(Transform bg)
+    {
+        Transform child = bg.GetChild(0);
+        Color c = child.GetComponent<Image>().color;
+        c.a = 0f;
+        child.GetComponent<Image>().color = c;
+
+        bg.gameObject.SetActive(true);
+
+        for (float f = 0.05f; f < 1.05; f += 0.05f)
+        {
+            c = bg.GetChild(0).GetComponent<Image>().color;
+            c.a = f;
+            bg.GetChild(0).GetComponent<Image>().color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
     }
 
     private void MiniGameStart(string name)
