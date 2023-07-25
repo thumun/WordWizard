8f64f6f5468c9a815567a8d2651bc219d7f97580
charlie-crawford <cmcrawford@haverford.edu>
2023-07-25 15:08:42 -0400

    added WASD movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D theRB;
+    public Rigidbody theRB;
     public float moveSpeed;
     public static PlayerController instance;
     public string lastAreaExit;
@@ -27,6 +27,23 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Basic movement code - will need to be expanded when art/animations are added 
-        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        // theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+
+        if (Input.GetKey(KeyCode.W)) // TODO - change this?
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-1 * Vector3.forward * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -1, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 1, 0);
+        }
     }
 }
